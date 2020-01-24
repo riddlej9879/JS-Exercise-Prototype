2eb9e8cff63d3dd1624150daa930aa7ed5b3b9f8
Josh Riddle <riddlej9879@yahoo.com>
2020-01-23 19:32:01 -0600

    All objectives completed

--- index.js
+++ index.js
@@ -39,9 +39,27 @@ Airplane.prototype.land = function () {
         + It should return a string with `name` and `age`. Example: "Mary, 50"
 */
 
-function Person() {
-
+function Person(name, age) {
+  this.name = name;
+  this.age = age;
+  this.stomach = [];
 }
+
+Person.prototype.eat = function (someFood) {
+  if (this.stomach[this.stomach.length - 1] > 9) {
+    return `${this.name} is full`;
+  } else {
+    return this.stomach.push(someFood);
+  }
+};
+
+Person.prototype.poop = function () {
+  this.stomach.length = 0;
+};
+
+Person.prototype.toString = function () {
+  return `${this.name} ${this.age}`;
+};
 
 /*
   TASK 2
@@ -57,10 +75,27 @@ function Person() {
         + The `drive` method should return a string "I ran out of fuel at x miles!" x being `odometer`.
 */
 
-function Car() {
-
+function Car(model, milesPerGallon) {
+  this.model = model;
+  this.milesPerGallon = milesPerGallon;
+  this.tank = 0;
+  this.odometer = 0;
 }
 
+Car.prototype.fill = function (gallons) {
+  return (this.tank += gallons);
+};
+
+Car.prototype.drive = function (distance) {
+  if (this.tank - distance / this.milesPerGallon >= 0) {
+    this.odometer += distance;
+    this.tank = this.tank - distance / this.milesPerGallon;
+  } else {
+    this.odometer += this.tank * this.milesPerGallon;
+    this.tank = 0;
+    return `I ran out of fuel at ${this.odometer} miles`;
+  }
+};
 /*
   TASK 3
     - Write a Baby constructor subclassing Person.
@@ -68,28 +103,52 @@ function Car() {
     - Besides the methods on Person.prototype, babies have the ability to `.play()`:
         + Should return a string "Playing with x", x being the favorite toy.
 */
-function Baby() {
-
+function Baby(name, age, favoriteToy) {
+  Person.call(this, name, age);
+  this.favoriteToy = favoriteToy;
 }
+Baby.prototype = Object.create(Person.prototype);
 
+Baby.prototype.play = function () {
+  console.log(this.favoriteToy);
+  return 'Playing with ' + this.favoriteToy;
+};
+
+this.eat = function (someFood) {
+  if (this.stomach[this.stomach.length - 1] < 10) {
+    this.stomach.push(someFood);
+  }
+};
+
+this.poop = function () {
+  this.stomach.length = 0;
+};
 /* 
   TASK 4
 
   In your own words explain the four principles for the "this" keyword below:
-  1. 
-  2. 
-  3. 
-  4. 
+  1. Default binding when used in "console.log(this)" it defaults to the owner of whatever function called console.log
+  2. Implicit binding when used in "console.log(this.variable)" the "this" keyword refers to the object before the dot.
+  3. Explicit binding makes a function use a particular case of the object so we use "this" specifically reference the object.
+  4. New binding when creating a new empty object the "this" keyword applies to the property of the newly created object.
 */
 
 
 ///////// END OF CHALLENGE /////////
 ///////// END OF CHALLENGE /////////
 ///////// END OF CHALLENGE /////////
-if (typeof exports !== 'undefined') {
-  module.exports = module.exports || {}
-  if (Airplane) { module.exports.Airplane = Airplane }
-  if (Person) { module.exports.Person = Person }
-  if (Car) { module.exports.Car = Car }
-  if (Baby) { module.exports.Baby = Baby }
+if (typeof exports !== "undefined") {
+  module.exports = module.exports || {};
+  if (Airplane) {
+    module.exports.Airplane = Airplane;
+  }
+  if (Person) {
+    module.exports.Person = Person;
+  }
+  if (Car) {
+    module.exports.Car = Car;
+  }
+  if (Baby) {
+    module.exports.Baby = Baby;
+  }
 }
